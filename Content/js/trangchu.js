c8fc5b0c7169a4ab76a73e46e06d812569cbfcb4
23dh110373 <23dh110373@st.huflit.edu.vn>
2024-11-06 09:25:19 -0800

    chung

---
+++ Content/js/trangchu.js
@@ -0,0 +1,50 @@
+﻿let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev button');
+let next = document.getElementById('next button');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+if (next) {
+    next.onclick = function () {
+        if (active + 1 > lengthItems) {
+            active = 0;
+        } else {
+            active = active + 1;
+        }
+
+        reloadSlider();
+    };
+}
+if (prev) {
+    prev.onclick = function () {
+        if (active - 1 < 0) {
+            active = lengthItems;
+        } else {
+            active = active - 1;
+        }
+        reloadSlider();
+    };
+}
+let refreshSlider = setInterval(() => { next.click() }, 3000);
+
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    console.log('checkLeft: ', checkLeft);
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => { next.click() }, 3000);
+
+}
+dots.forEach((li, key) => {
+    li.addEventListener('click', function () {
+        active = key;
+        reloadSlider();
+    })
+})
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DeAnWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeAnWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6e76ebb0-1676-4ee8-b62b-4aa7389ea136")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
